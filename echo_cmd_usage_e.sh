a5225bced3374e87b01902b4a067c117971aed5e
Sumesh Gupta <skgs01@gmail.com>
2023-02-25 22:07:14 +0530

    echo command usage with flags

---
+++ echo_cmd_usage_e.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+###
+#   author : Sumesh
+#   version: v1.1
+#   this script will demonstrate usage of echo cmd
+#   echo [-enE] [arguments]
+#   echo -e => backslash escaped character will be interpreted like \n => new line
+
+
+echo "This is a simple msg"
+echo "This is one more line"
+
+# we can write one line using escape character sequence
+echo -e "This is a simple msg \nThis is one more line"
+
+
+
---
+++ echo_cmd_usage_n.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+###
+#   author : Sumesh
+#   version: v1.1
+#   this script will demonstrate usage of echo cmd
+#   echo [-enE] [arguments]
+#   echo -n => the trailing new line is suppressed
+
+echo "This is a simple msg"
+echo "This is one more line"
+echo -n "Today date is : $(date)"
+echo "Current logged in user is : $USER"
+
+
+
