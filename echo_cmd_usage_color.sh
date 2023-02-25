b6225c5195f31cfd6dae23f838291c5df90ed011
Sumesh Gupta <skgs01@gmail.com>
2023-02-25 22:39:21 +0530

    echo command usage with colorfultext

---
+++ echo_cmd_usage_color.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+###
+#   author : Sumesh
+#   version: v1.1
+#   this script will demonstrate usage of echo cmd
+#   echo [-enE] [arguments]
+#   echo -e => color code font weight 033[0;31m 
+
+
+echo -e "\033[0;31mHello Sumesh See the color\033[0m"
+echo -e "\033[1;31mHello Sumesh See the color\033[0m"
+echo -e "\033[4;31mHello Sumesh See the color\033[0m"
+
+
