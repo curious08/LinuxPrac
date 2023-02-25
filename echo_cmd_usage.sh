e2b5a0ebdaa78f3a376ec26f3f2270e6bbecb844
Sumesh Gupta <skgs01@gmail.com>
2023-02-25 21:46:59 +0530

    demo of echo command
    example 1

---
+++ echo_cmd_usage.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+###
+#   author : Sumesh
+#   version: v1.0
+#   this script will demonstrate usage of echo cmd
+#
+#
+
+echo "This is a simple msg"
+echo "This is one more line"
+echo "Today date is : $(date)"
+echo "Current logged in user is : $USER"
+
+
+
